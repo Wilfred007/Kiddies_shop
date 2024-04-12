ab03fa97c784ad6e8607ec3ea5bb6219c1cadd42
Wilfred007 <adzerwilfred007@gmail.com>
2024-04-12 14:03:49 -0700

    Home

---
+++ src/config/index.ts
@@ -0,0 +1,49 @@
+export const PRODUCT_CATEGORIES  =  [
+  {
+    label: "Kiddies Toys",
+    value: "kiddies_toys" as const, 
+    feature: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: '/nav/kiddies-toys/mixed.jpg',
+      },
+
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: '/nav/kiddies-toys/mixed.jpg',
+      }, 
+
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: '/nav/kiddies-toys/mixed.jpg',
+      }
+    ]
+  },
+
+  {
+    label: "Toys",
+    value: "toys" as const, 
+    feature: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: '/nav/toys/toy1.jpg',
+      },
+
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: '/nav/toys/toy2.jpg',
+      }, 
+
+      {
+        name: "Best Selling Toys",
+        href: "#",
+        imageSrc: '/nav/toys/toy3.jpg',
+      }
+    ]
+  }
+]
